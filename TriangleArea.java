3ca78ecaaf860e7f3af9f144ac68206c051ddbc3
AshwathiAnand <54394925+AshwathiAnand@users.noreply.github.com>
2020-07-03 15:42:13 +0530

    Add files via upload

---
+++ TriangleArea.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class TriangleArea {
+	public static void main(String args[]) {
+		if((args.length != 0) && (args.length == 2)) {
+			System.out.print(args[0] + " " + args[1]);
+			double base = Double.parseDouble(args[0]);
+			double height = Double.parseDouble(args[1]);
+			double area = (base * height) / 2;	
+					System.out.print("\n" + area);
+		}		
+	}
+}
